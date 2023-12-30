15bad0fd31bf9bd8bec0a01750c3ccf75fc78842
asGabriel <andrade.gabriel2295@gmail.com>
2023-12-30 04:35:41 -0300

    refac: strings done

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -7,17 +7,20 @@
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    input.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    // ???
+    let mut word = String::from(input);
+    word.push_str(" world!");
+    word
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.replace("cars", "balloons")
 }
 
 #[cfg(test)]
